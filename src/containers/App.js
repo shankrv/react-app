d8f535d5102501465011b1c9188327b9456a034c
Ravi Shankar <67696330+shankrv@users.noreply.github.com>
2020-08-14 17:10:37 +0530

    comm-indent

--- src/containers/App.js
+++ src/containers/App.js
@@ -103,43 +103,43 @@ export default App;
 
 /*
 switchName = (name) => {
-	// Don't mutate the state directly. Use setState()
-	// this.state.persons[0].name = 'Ryan';
-	this.setState({
-		persons: [
-			{ name: 'Ryan', age: 23 },
-			{ name: name, age: 38 },
-			{ name: 'Andy', age: 22 },
-		]
-	});
+  // Don't mutate the state directly. Use setState()
+  // this.state.persons[0].name = 'Ryan';
+  this.setState({
+    persons: [
+      { name: 'Ryan', age: 23 },
+      { name: name, age: 38 },
+      { name: 'Andy', age: 22 },
+    ]
+  });
 }
 
 changeName = (event) => { // event -> obj
-	this.setState({
-		persons: [
-			{ name: 'Ryan', age: 23 },
-			{ name: event.target.value, age: 38 }, // target elem.
-			{ name: 'Andy', age: 22 },
-		]
-	});
+  this.setState({
+    persons: [
+      { name: 'Ryan', age: 23 },
+      { name: event.target.value, age: 38 }, // target elem.
+      { name: 'Andy', age: 22 },
+    ]
+  });
 }
 
 
 // inline-styling
 const style = { // style -> obj
-	backgroundColor: 'white', // camelCase for '-'
-	font: 'inherit',
-	border: '1px solid deepskyblue',
-	padding: '8px'
+  backgroundColor: 'white', // camelCase for '-'
+  font: 'inherit',
+  border: '1px solid deepskyblue',
+  padding: '8px'
 }
 
 const jsx =
-	(<div>
-		<Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Hobbies : Gaming </Person>
-		<Person click={this.switchName.bind(this, 'Mike -> bind()')} name={this.state.persons[1].name} age={this.state.persons[1].age} change={this.changeName} />
-		<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-		<button style={style} onClick={() => this.switchName('Mike -> arrow-fx')}> Switch Name </button>
-	</div>);
+  (<div>
+    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Hobbies : Gaming </Person>
+    <Person click={this.switchName.bind(this, 'Mike -> bind()')} name={this.state.persons[1].name} age={this.state.persons[1].age} change={this.changeName} />
+    <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+    <button style={style} onClick={() => this.switchName('Mike -> arrow-fx')}> Switch Name </button>
+  </div>);
 */
 
 
@@ -147,39 +147,39 @@ const jsx =
 /*
 const App = () => {
 
-	// react - hook : useState() w. callback-fx
-	const [personState, setPerson] = useState({ // arr. destr
-		persons: [ // init-state
-			{ name: 'Ravi', age: 26 },
-			{ name: 'John', age: 30 },
-			{ name: 'Jane', age: 28 },
-		]
-	});
+  // react - hook : useState() w. callback-fx
+  const [personState, setPerson] = useState({ // arr. destr
+    persons: [ // init-state
+      { name: 'Ravi', age: 26 },
+      { name: 'John', age: 30 },
+      { name: 'Jane', age: 28 },
+    ]
+  });
 
-	const switchName = () => { // eventHandler method
-		setPerson({ // fx-callback to update new state
-			persons: [
-				{ name: 'Ryan', age: 23 },
-				{ name: 'Mike', age: 38 },
-				{ name: 'Andy', age: 22 },
-			]
-			// mutates init-state by replace and re-renders the UI
-		});
-	}
+  const switchName = () => { // eventHandler method
+    setPerson({ // fx-callback to update new state
+      persons: [
+        { name: 'Ryan', age: 23 },
+        { name: 'Mike', age: 38 },
+        { name: 'Andy', age: 22 },
+      ]
+      // mutates init-state by replace and re-renders the UI
+    });
+  }
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1 className="App-title">Welcome to React</h1>
-			</header>
-			<h3>Hello World.</h3>
-			<Person name={personState.persons[0].name} age={personState.persons[0].age}> Hobbies : Gaming </Person>
-			<Person name={personState.persons[1].name} age={personState.persons[1].age} />
-			<Person name={personState.persons[2].name} age={personState.persons[2].age} />
-			<button onClick={switchName}> Switch Name </button>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <h3>Hello World.</h3>
+      <Person name={personState.persons[0].name} age={personState.persons[0].age}> Hobbies : Gaming </Person>
+      <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+      <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+      <button onClick={switchName}> Switch Name </button>
+    </div>
+  );
 
 }
 */
