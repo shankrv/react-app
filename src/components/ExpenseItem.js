cf5babf657017dec53b791f59e59bad2e3bb114b
shankrv <ravis.webd@gmail.com>
2023-01-19 01:11:07 +0530

    First Component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>January 18th 2023</div>
+      <div>
+        <h3>Rent</h3>
+        <div>₹10,000.00</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
