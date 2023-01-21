81ba5c1effabaa3a30a7809aa8ff69b7bb6bfc61
shankrv <ravis.webd@gmail.com>
2023-01-22 01:16:39 +0530

    Assignment-1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -9,11 +9,8 @@ function App() {
   ];
   return (
     <div>
-      <h2>Create React App</h2>
-      <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}></ExpenseItem>
-      <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}></ExpenseItem>
-      <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}></ExpenseItem>
-      <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount}></ExpenseItem>
+      <h2>Expense Tracker</h2>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
