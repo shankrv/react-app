e11cd40901923da2b9e55b42e11d8253be3e25e9
shankrv <ravis.webd@gmail.com>
2023-01-21 23:56:01 +0530

    passing dynamic data to JSX via expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseItem = {
+    date: new Date(2023, 0, 18).toISOString(),
+    title: 'Rent',
+    price: '10,000.00',
+  };
   return (
     <div className="expense-item">
-      <div>January 18th 2023</div>
+      <div>{expenseItem.date}</div>
       <div className="expense-item__description">
-        <h3>Rent</h3>
-        <div className="expense-item__price">₹10,000.00</div>
+        <h3>{expenseItem.title}</h3>
+        <div className="expense-item__price">₹{expenseItem.price}</div>
       </div>
     </div>
   );
