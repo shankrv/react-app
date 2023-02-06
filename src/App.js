e0d96e0e3bd3101e7179c4080b1e20d561e2862f
shankrv <ravis.webd@gmail.com>
2023-02-06 19:50:46 +0530

    Goal Tracker App: Starter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/** --- Expense Tracker --- **
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/Expenses/NewExpense';
@@ -24,6 +25,61 @@ function App() {
       <h2>Expense Tracker</h2>
       <NewExpense count={expenses.length} onNewExpense={newExpenseHandler} />
       <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
+*/
+
+/** --- Goal Tracker --- */
+import './App.css';
+import { useState } from 'react';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+
+function App() {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' },
+  ]);
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>;
+
+  if (courseGoals.length > 0) {
+    content = <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />;
+  }
+
+  return (
+    <div>
+      <section id='goal-form'>
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id='goals'>
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
     </div>
   );
 }
---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,28 @@
+import './CourseInput.css';
+import Button from './../../General/Button';
+import { useState } from 'react';
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className='form-control'>
+        <label>Course Goal</label>
+        <input type='text' onChange={goalInputChangeHandler} />
+      </div>
+      <Button type='submit'>Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
