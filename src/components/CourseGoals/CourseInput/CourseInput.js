78e95845f722acc4f3f40951ad513acb1304563e
shankrv <ravis.webd@gmail.com>
2023-02-07 18:21:49 +0530

    setting dynamic styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,20 +3,23 @@ import Button from './../../General/Button';
 import { useState } from 'react';
 
 const CourseInput = (props) => {
+  const [isValid, setValidity] = useState(true);
   const [enteredValue, setEnteredValue] = useState('');
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length) setValidity(true);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (!enteredValue.trim().length) return setValidity(false);
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
       </div>
