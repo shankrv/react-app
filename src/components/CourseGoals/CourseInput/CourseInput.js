bff5f797bea72a017cfe696bc0fb172c6077ef0f
shankrv <ravis.webd@gmail.com>
2023-02-07 20:02:12 +0530

    styled-components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,6 +1,37 @@
 import './CourseInput.css';
 import Button from './../../General/Button';
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? '#c92a2a' : '#343a40')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    background: ${(props) => (props.invalid ? '#fff5f5' : '#f8f9fa')};
+  }
+
+  & input:focus {
+    outline: none;
+    background: #f8f9fa;
+    border-color: #343a40;
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
+`;
 
 const CourseInput = (props) => {
   const [isValid, setValidity] = useState(true);
@@ -19,10 +50,10 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type='submit'>Add Goal</Button>
     </form>
   );
--- src/components/General/Button.js
+++ src/components/General/Button.js
@@ -1,3 +1,4 @@
+/** --- Functional Component --- **
 import './Button.css';
 
 const Button = (props) => {
@@ -7,5 +8,30 @@ const Button = (props) => {
     </button>
   );
 };
+*/
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #e9ecef;
+  color: white;
+  background: #343a40;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    color: #3a3a3a;
+    background: #f1f3f5;
+    border-color: #3a3a3a;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
