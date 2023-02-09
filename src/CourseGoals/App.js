ef1d9624a947f474be37b221d61fe259327ca4ec
shankrv <ravis.webd@gmail.com>
2023-02-09 20:55:14 +0530

    organized CourseGoals and ExpenseTracker project files

--- src/CourseGoals/App.js
+++ src/CourseGoals/App.js
@@ -1,38 +1,3 @@
-/** --- Expense Tracker --- **
-import './App.css';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/Expenses/NewExpense';
-import { useState } from 'react';
-
-const initExpenses = [
-  { id: 'E-1', date: new Date(2022, 5, 18), title: 'Rent', amount: 5000 },
-  { id: 'E-2', date: new Date(2023, 0, 19), title: 'Maintenance', amount: 1500 },
-  { id: 'E-3', date: new Date(2023, 1, 20), title: 'Electric Bill', amount: 1250 },
-  { id: 'E-4', date: new Date(2022, 11, 21), title: 'Internet', amount: 3560 },
-  { id: 'E-5', date: new Date(2020, 2, 9), title: 'Health Insurance', amount: 2500 },
-];
-
-function App() {
-  const [expenses, setExpense] = useState(initExpenses);
-
-  const newExpenseHandler = (expense) => {
-    console.log('in App.js: ', expense);
-    setExpense((expenseList) => [expense, ...expenseList]);
-  };
-
-  return (
-    <div>
-      <h2>Expense Tracker</h2>
-      <NewExpense count={expenses.length} onNewExpense={newExpenseHandler} />
-      <Expenses expenses={expenses} />
-    </div>
-  );
-}
-
-export default App;
-*/
-
-/** --- Goal Tracker --- */
 import './App.css';
 import { useState } from 'react';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import CourseGoalsApp from './CourseGoals/App';
+import ExpenseTrackerApp from './ExpenseTracker/App';
+
+const { REACT_APP_PROJECT } = process.env;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+switch (REACT_APP_PROJECT) {
+  case 'COURSE':
+    root.render(<CourseGoalsApp />);
+    break;
+
+  case 'EXPENSE':
+    root.render(<ExpenseTrackerApp />);
+    break;
+
+  default:
+    root.render(<CourseGoalsApp />);
+    break;
+}
