8499b4b602c441f7caca5ef5e853388200f393b3
shankrv <ravis.webd@gmail.com>
2023-04-01 01:38:06 +0530

    Class-based components with state and events

--- src/UserList/components/User.js
+++ src/UserList/components/User.js
@@ -1,5 +1,15 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => <li className={classes.user}>{props.name}</li>;
+// Class-based Component
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// Functional Component
+// const User = (props) => <li className={classes.user}>{props.name}</li>;
 
 export default User;
--- src/UserList/components/Users.js
+++ src/UserList/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 
 import classes from './Users.module.css';
 
@@ -10,6 +10,39 @@ const USERS = [
   { id: 'U-3', name: 'David' },
 ];
 
+const usersList = (
+  <ul>
+    {USERS.map((user) => (
+      <User key={user.id} name={user.name} />
+    ))}
+  </ul>
+);
+
+// Class-based Component
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = { showUsers: false };
+  }
+
+  toggleHandler() {
+    this.setState((currentState) => {
+      return { showUsers: !currentState.showUsers };
+    });
+  }
+
+  render() {
+    return (
+      <div className={classes.users}>
+        {this.state.showUsers && usersList}
+        <button onClick={this.toggleHandler.bind(this)}>{this.state.showUsers ? 'Hide' : 'Show'} Users</button>
+      </div>
+    );
+  }
+}
+
+// Functional Component
+/*
 const Users = (props) => {
   const [showUsers, setShowUsers] = useState(true);
 
@@ -30,5 +63,6 @@ const Users = (props) => {
     </div>
   );
 };
+*/
 
 export default Users;
