03d842b3bf71d121ac66aabad31c93be23d49ba2
shankrv <ravis.webd@gmail.com>
2023-04-08 12:14:27 +0530

    loader and http-error handling

--- src/StarWars/App.js
+++ src/StarWars/App.js
@@ -7,9 +7,13 @@ import Movies from './components/Movies';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function listMovies() {
     try {
+      setError(null);
+      setIsLoading(true);
       const { data } = await axios.get('https://swapi.dev/api/films');
       const films = data.results.map((film) => {
         return { id: film.episode_id, title: film.title, release: film.release_date, description: film.opening_crawl };
@@ -17,13 +21,16 @@ function App() {
       setMovies(films);
     } catch (error) {
       console.error(error);
+      setError(error.message);
     }
+    setIsLoading(false);
   }
 
   return (
     <Fragment>
       <section>
-        <Movies movies={movies} />
+        {!isLoading && error && <p>Something went wrong!</p>}
+        {isLoading ? <p>Loading...</p> : <Movies movies={movies} />}
       </section>
       <section>
         <button onClick={listMovies}>List Movies</button>
