786511ec33c1a0f0fff58b9cdc9d8f67043c4160
shankrv <ravis.webd@gmail.com>
2023-04-23 15:06:37 +0530

    added basic validations on form-submit

--- src/UserInput/components/Input.js
+++ src/UserInput/components/Input.js
@@ -1,9 +1,28 @@
+import { useRef, useState } from 'react';
+
 const Input = (props) => {
+  const nameRef = useRef();
+  const [isNameValid, setIsNameValid] = useState(false);
+  const [isNameTouched, setIsNameTouched] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setIsNameTouched(true);
+    const name = nameRef.current.value.trim();
+    if (name.length) setIsNameValid(true);
+    else setIsNameValid(false);
+    event.target.reset();
+  };
+
+  const isNameInputInvalid = isNameTouched && !isNameValid;
+  const nameInputClass = isNameInputInvalid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={submitHandler}>
+      <div className={nameInputClass}>
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameRef} />
+        {isNameInputInvalid && <p className='error-text'>Name must be valid.</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
