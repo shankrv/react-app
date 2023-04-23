c89cab8e826cb029e61d02524ce412d5ece71977
shankrv <ravis.webd@gmail.com>
2023-04-23 16:10:41 +0530

    updated validation on lost focus

--- src/UserInput/components/Input.js
+++ src/UserInput/components/Input.js
@@ -14,6 +14,13 @@ const Input = (props) => {
     event.target.reset();
   };
 
+  const nameBlurHandler = (event) => {
+    setIsNameTouched(true);
+    const name = nameRef.current.value.trim();
+    if (name.length) setIsNameValid(true);
+    else setIsNameValid(false);
+  };
+
   const isNameInputInvalid = isNameTouched && !isNameValid;
   const nameInputClass = isNameInputInvalid ? 'form-control invalid' : 'form-control';
 
@@ -21,7 +28,7 @@ const Input = (props) => {
     <form onSubmit={submitHandler}>
       <div className={nameInputClass}>
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' ref={nameRef} />
+        <input type='text' id='name' ref={nameRef} onBlur={nameBlurHandler} />
         {isNameInputInvalid && <p className='error-text'>Name must be valid.</p>}
       </div>
       <div className='form-actions'>
