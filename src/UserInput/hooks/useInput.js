1b86b099a2323b48f2ea441072057cead09403f0
shankrv <ravis.webd@gmail.com>
2023-05-01 23:57:32 +0530

    working with `useReducer` in `useInput` custom-hook

--- src/UserInput/hooks/useInput.js
+++ src/UserInput/hooks/useInput.js
@@ -1,21 +1,32 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const inputReducer = (state, action) => {
+  if (action.type === 'blur') return { value: state.value, isTouched: true };
+  if (action.type === 'input') return { value: action.value, isTouched: state.isTouched };
+  if (action.type === 'reset') return { value: '', isTouched: false };
+  return state;
+};
 
 const useInput = (validator) => {
-  const [value, setValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  // const [value, setValue] = useState('');
+  // const [isTouched, setIsTouched] = useState(false);
+  const [state, dispatch] = useReducer(inputReducer, { value: '', isTouched: false });
 
-  const isValid = validator(value);
-  const hasError = isTouched && !isValid;
+  const isValid = validator(state.value);
+  const hasError = state.isTouched && !isValid;
 
-  const blurHandler = () => setIsTouched(true);
-  const changeHandler = (event) => setValue(event.target.value);
+  // const blurHandler = () => setIsTouched(true);
+  // const changeHandler = (event) => setValue(event.target.value);
+  const blurHandler = () => dispatch({ type: 'blur' });
+  const changeHandler = (event) => dispatch({ type: 'input', value: event.target.value });
 
   const reset = () => {
-    setValue('');
-    setIsTouched(false);
+    // setValue('');
+    // setIsTouched(false);
+    dispatch({ type: 'reset' });
   };
 
-  return { value, isValid, hasError, blurHandler, changeHandler, reset };
+  return { value: state.value, isValid, hasError, blurHandler, changeHandler, reset };
 };
 
 export default useInput;
