831c4a8cd2654b7db273d7016d8a8b28cfc5ccf4
shankrv <ravis.webd@gmail.com>
2024-03-10 17:49:46 +0530

    output dynamic values and content

--- src/Essentials/App.jsx
+++ src/Essentials/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import reactAtomIcon from './assets/react-atom-icon.png';
 
+const description = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
       <img src={reactAtomIcon} alt='react-atom-icon' />
       <h1>React Essentials</h1>
-      <p>Fundamental React concepts we will need for almost any app to build!</p>
+      <p>{description[getRandomInt(2)]} React concepts we will need for almost any app to build!</p>
     </header>
   );
 }
