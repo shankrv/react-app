07e40bbf885a0e5be7e2d67415a493e98cfa235e
shankrv <ravis.webd@gmail.com>
2024-03-10 17:11:00 +0530

    creating a custom component

--- src/Essentials/App.jsx
+++ src/Essentials/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
 import reactAtomIcon from './assets/react-atom-icon.png';
 
+function Header() {
+  return (
+    <header>
+      <img src={reactAtomIcon} alt='react-atom-icon' />
+      <h1>React Essentials</h1>
+      <p>Fundamental React concepts we will need for almost any app to build!</p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src={reactAtomIcon} alt='react-atom-icon' />
-        <h1>React Essentials</h1>
-        <p>Fundamental React concepts we will need for almost any app to build!</p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
