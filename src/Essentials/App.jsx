1fed27e692542aba8922f0a0d593cb2af4c75d65
shankrv <ravis.webd@gmail.com>
2024-03-10 22:55:26 +0530

    events, handlers and state-management

--- src/Essentials/App.jsx
+++ src/Essentials/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 import './App.css';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 function App() {
+  const [selected, setSelected] = useState('components');
+
+  function selectHandler(selection) {
+    setSelected(selection);
+  }
+
   return (
     <div>
       <Header />
@@ -17,6 +26,22 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => selectHandler('components')}>Components</TabButton>
+            <TabButton onClick={() => selectHandler('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => selectHandler('props')}>Props</TabButton>
+            <TabButton onClick={() => selectHandler('state')}>State</TabButton>
+          </menu>
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selected].title}</h3>
+            <p>{EXAMPLES[selected].description}</p>
+            <pre>
+              <code>{EXAMPLES[selected].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
