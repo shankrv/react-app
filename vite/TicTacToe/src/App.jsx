66504631dc351a40ad679546d15e74d4fc940ec8
shankrv <ravis.webd@gmail.com>
2024-03-17 01:00:22 +0530

    setting up the player components

--- vite/TicTacToe/src/App.jsx
+++ vite/TicTacToe/src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import Player from './components/Player';
 
 function App() {
-  return <p>Coming soon...</p>;
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <Player name='Player - 1' symbol='X' />
+          <Player name='Player - 2' symbol='0' />
+        </ol>
+        GAMEBOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
---
+++ vite/TicTacToe/src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function clickHandler() {
+    setEditing((editing) => !editing);
+  }
+
+  function changeHandler(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className='player'>
+        {isEditing ? (
+          <input type='text' required value={playerName} onChange={changeHandler} />
+        ) : (
+          <span className='player-name'>{playerName}</span>
+        )}
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={clickHandler}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
