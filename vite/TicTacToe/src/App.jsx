72d03a8594518d059315ce017f2c217eae1b236b
shankrv <ravis.webd@gmail.com>
2024-03-19 00:34:07 +0530

    render multi-dimensional `3x3` lists

--- vite/TicTacToe/src/App.jsx
+++ vite/TicTacToe/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
           <Player name='Player - 1' symbol='X' />
           <Player name='Player - 2' symbol='0' />
         </ol>
-        GAMEBOARD
+        <GameBoard />
       </div>
       LOG
     </main>
