71f68f5d4d293c189aa7032f5ca8131973d89cf4
shankrv <ravis.webd@gmail.com>
2024-03-21 21:43:44 +0530

    FinalCountdown App: Starter

---
+++ vite/FinalCountdown/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Player from './components/Player';
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id='challenges'></div>
+    </>
+  );
+}
+
+export default App;
