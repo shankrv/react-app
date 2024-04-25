49fe94b848a9faccbeccb57116e39d1834fecd00
shankrv <ravis.webd@gmail.com>
2024-04-25 21:18:11 +0530

    managing user-input via state

--- vite/src/ReactForms/components/Login.jsx
+++ vite/src/ReactForms/components/Login.jsx
@@ -1,17 +1,36 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [input, setInput] = useState({ email: '', password: '' });
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log(input);
+  }
+
+  function changeHandler(id, event) {
+    setInput((prev) => ({ ...prev, [id]: event.target.value }));
+  }
+
   return (
-    <form>
+    <form onSubmit={(e) => submitHandler(e)}>
       <h2>Login</h2>
 
       <div className='control-row'>
         <div className='control no-margin'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' id='email' />
+          <input type='email' name='email' id='email' value={input.email} onChange={(e) => changeHandler('email', e)} />
         </div>
 
         <div className='control no-margin'>
           <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={input.password}
+            onChange={(e) => changeHandler('password', e)}
+          />
         </div>
       </div>
 
